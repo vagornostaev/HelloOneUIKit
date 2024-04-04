7d8d5d743e7915c87bc2150f01c15059a7f35722
Vadim <vagornostaev@mail.ru>
2024-04-04 22:36:03 +0300

    Initial Commit

---
+++ HelloOneUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloOneUIKit
+//
+//  Created by Вадим Горностаев on 04.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
